a5666f175dae3567e3c4c9785ace9045082a09bb
jmarco26 <116202850+jmarco26@users.noreply.github.com>
2024-05-09 15:33:19 +0800

    Tool 2 with Forms.
    Might need a QA's opinion for improvements on the UX.

---
+++ IdentifyingPAN_Tool2/IdentifyingPAN_Tool2/Models/DatJournal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool2_Console.Models
+{
+    internal class DatJournal
+    {
+        public string Title { get; set; }
+        public string ISSN {  get; set; }
+        public string eISSN { get; set; }
+    }
+}
